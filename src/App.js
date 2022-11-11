6c71316e0a752f641d2ac062dd024e1d9a2bf582
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2022-11-11 19:09:46 +0700

    get surah app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { GetAllSurah } from "./providers/quran.provider";
 function App() {
+	const [surah, setSurah] = useState([]);
+
+	useEffect(() => {
+		GetAllSurah().then((response) => {
+			setSurah(response.data.data)
+			console.log(response.data.data)
+		})
+	}, []);
   return (
     <>
-      <h1>hello world</h1>
+      <table>
+          <thead>
+            <tr>
+              <th>No</th>
+            </tr>
+          </thead>
+          <tbody>
+            {surah.map((data, index) => (
+              <tr key={index}>
+                <td>{data.number}</td>
+								<td>{data.name.transliteration.id}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
     </>
   );
 }
