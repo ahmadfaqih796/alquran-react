1a1755aab9d20d5b1c04cd52974855b5fcc9d1b4
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2022-11-16 09:30:54 +0700

    styling navigasi

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -3,6 +3,9 @@ export const Navigasi = () => {
   return (
     <>
       <nav>
+				<div className="logo">
+					<h1>hello</h1>
+				</div>
         <ul>
           <li>Home</li>
           <li>About</li>
