79b37aeaf01a5b5d87ebb52a7e8a726f56850f90
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2022-12-12 09:29:30 +0700

    fix style template

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -6,7 +6,20 @@ export const Navigasi = () => {
       <main className="tampilan">
         <aside className="sidebar-kiri"></aside>
         <div className="aaa">
-          
+          <nav>
+            <ul>
+              <li>
+                <a href="#!">Qur'an</a>
+              </li>
+              <li>
+                <a href="#!">Hadits</a>
+              </li>
+            </ul>
+            <main className="pencarian">
+              <input type="text" placeholder="cari surah" />
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </main>
+          </nav>
           <article className="konten">
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
