4dee1078f699ed197ce713f4345af4ac1caf229a
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2022-12-12 09:22:39 +0700

    update style

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -5,20 +5,17 @@ export const Navigasi = () => {
     <>
       <main className="tampilan">
         <aside className="sidebar-kiri"></aside>
-        <nav>
-          <ul>
-            <li>
-              <a href="#!">Qur'an</a>
-            </li>
-            <li>
-              <a href="#!">Hadits</a>
-            </li>
-          </ul>
-          <main className="pencarian">
-            <input type="text" placeholder="cari surah" />
-            <i class="fa-solid fa-magnifying-glass"></i>
-          </main>
-        </nav>
+        <div className="aaa">
+          
+          <article className="konten">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
+              dignissimos praesentium maxime, consequatur impedit obcaecati vero
+              accusamus vel atque nobis eum officiis doloremque illo sapiente,
+              quaerat ullam aut voluptate ducimus.
+            </p>
+          </article>
+        </div>
         <aside className="sidebar-kanan"></aside>
       </main>
     </>
