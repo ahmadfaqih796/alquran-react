e33e9299ef635babd2e2b53bb9343dd9f11a1ac3
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2022-12-12 09:11:21 +0700

    styling sidebar

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -3,22 +3,24 @@ import "../assets/css/sidebar.css";
 export const Navigasi = () => {
   return (
     <>
-      <aside className="sidebar-kiri"></aside>
-      <nav>
-        <ul>
-          <li>
-            <a href="#!">Qur'an</a>
-          </li>
-          <li>
-            <a href="#!">Hadits</a>
-          </li>
-        </ul>
-        <main className="pencarian">
-          <input type="text" placeholder="cari surah" />
-          <i class="fa-solid fa-magnifying-glass"></i>
-        </main>
-      </nav>
-      <aside className="sidebar-kanan"></aside>
+      <main className="tampilan">
+        <aside className="sidebar-kiri"></aside>
+        <nav>
+          <ul>
+            <li>
+              <a href="#!">Qur'an</a>
+            </li>
+            <li>
+              <a href="#!">Hadits</a>
+            </li>
+          </ul>
+          <main className="pencarian">
+            <input type="text" placeholder="cari surah" />
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </main>
+        </nav>
+        <aside className="sidebar-kanan"></aside>
+      </main>
     </>
   );
 };
