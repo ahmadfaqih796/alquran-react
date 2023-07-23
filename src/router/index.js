2c01bbc21579493764cff2cbe4d2b55d21e1fade
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-23 11:19:20 +0700

    error page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Navigasi } from "../components/Navbar.component";
+import ErrorPage from "../pages/ErrorPage";
 import Surah from "../pages/Surah";
 
 export const ROUTER = createBrowserRouter([
   {
     path: "/",
     element: <Surah />,
+    errorElement: <ErrorPage />,
   },
 ]);
